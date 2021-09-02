aa7aa9b3a07c99bda7b5900449fdd666ba428754
parksaneon <sangeon1500@daum.net>
2021-09-02 14:48:23 +0900

    add data model

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 import * as express from 'express';
+import { Cat, CatType } from 'app.model';
 
-const app: express.Express = express(); // express의 인스턴스 객체 - 서버의 역활
-const port: number = 8000;
+const app: express.Express = express(); // express의 인스턴스 객체(서버의 역활)
 
 app.get('/', (req: express.Request, res: express.Response) => {
   // 웹에서 / 로 요청을 했을 때 동작을 적어주는 callback 함수
-  res.send('hi'); // 요청에 대한 응답을 client한테 보낸다
+  console.log(req);
+  // 요청에 대한 응답을 client한테 보낸다
+  res.send({ cats: Cat });
 });
 
-app.listen(port, () => {
-  // listen - 서버를 여는 메서드
-  console.log(`listening at ${port}port`);
+app.listen(8000, () => {
+  console.log('server is on');
 });
