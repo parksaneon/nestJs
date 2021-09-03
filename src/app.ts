6eac7ea0b4b6fead9c7eb6fa01789d78fdb4abb7
parksaneon <sangeon1500@daum.net>
2021-09-03 14:50:42 +0900

    make Server Class and init

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,48 @@
 import * as express from 'express';
 import catsRouter from 'cats/cats.route';
 
-const app: express.Express = express(); // express의 인스턴스 객체(서버의 역활)
+class Server {
+  public app: express.Application;
 
-// 미들웨어 : 양쪽을 연결해서 데이터를 주고받을 수 있도록 중간에서 매개체 역활을 하는 것
-// 모든 요청은 미들웨어를 거쳐서 온다
-app.use((req, res, next) => {
-  console.log('logging middleware');
-  next();
-});
+  constructor() {
+    const app: express.Application = express(); // express의 인스턴스 객체(서버의 역활)
+    this.app = app;
+  }
 
-// json 미들웨어 - express 에서 json데이터를 인식가능 하게 해줌
-app.use(express.json());
+  private setRoute() {
+    this.app.use(catsRouter);
+  }
 
-app.use(catsRouter);
+  private setMiddleWares() {
+    // 미들웨어 : 양쪽을 연결해서 데이터를 주고받을 수 있도록 중간에서 매개체 역활을 하는 것
+    // 모든 요청은 미들웨어를 거쳐서 온다
+    this.app.use((req, res, next) => {
+      console.log('logging middleware');
+      next();
+    });
 
-// 404 미들웨어
-app.use((req, res, next) => {
-  res.send({ error: '404 not found error' });
-});
+    // json 미들웨어 - express 에서 json데이터를 인식가능 하게 해줌
+    this.app.use(express.json());
 
-app.listen(8000, () => {
-  console.log('server is on');
-});
+    this.setRoute();
+
+    // 404 미들웨어
+    this.app.use((req, res, next) => {
+      res.send({ error: '404 not found error' });
+    });
+  }
+
+  public listen() {
+    this.setMiddleWares();
+    this.app.listen(8000, () => {
+      console.log('server is on');
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
